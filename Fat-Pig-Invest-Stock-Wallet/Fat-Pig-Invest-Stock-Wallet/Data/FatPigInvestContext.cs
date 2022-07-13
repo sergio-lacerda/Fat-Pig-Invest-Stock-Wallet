fc27a7f70ed86463d4a22d699df703696fae742e
Sérgio Lacerda <cursos@sergiolacerda.com.br>
2022-07-13 12:11:16 -0300

    Testing initial migration for base models

--- Fat-Pig-Invest-Stock-Wallet/Fat-Pig-Invest-Stock-Wallet/Data/FatPigInvestContext.cs
+++ Fat-Pig-Invest-Stock-Wallet/Fat-Pig-Invest-Stock-Wallet/Data/FatPigInvestContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fat_Pig_Invest_Stock_Wallet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fat_Pig_Invest_Stock_Wallet.Data
 {
@@ -7,5 +8,11 @@ namespace Fat_Pig_Invest_Stock_Wallet.Data
         public FatPigInvestContext(DbContextOptions<FatPigInvestContext> options) : base(options)
         {
         }
+
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Corretora> Corretoras { get; set; }
+        public DbSet<Acao> Acoes { get; set; }
+        public DbSet<Nota> Notas { get; set; }
+        public DbSet<Ordem> Ordens { get; set; }
     }
 }
---
+++ Fat-Pig-Invest-Stock-Wallet/Fat-Pig-Invest-Stock-Wallet/Migrations/20220713150701_Migracao-inicial.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fat_Pig_Invest_Stock_Wallet.Migrations
+{
+    public partial class Migracaoinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Corretoras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corretoras", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Empresas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cnpj = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Notas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CorretoraId = table.Column<int>(type: "int", nullable: false),
+                    NumeroNota = table.Column<int>(type: "int", nullable: false),
+                    DataPregao = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    PrecoUnitario = table.Column<double>(type: "double", nullable: false),
+                    Emolumentos = table.Column<double>(type: "double", nullable: false),
+                    Corretagem = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notas_Corretoras_CorretoraId",
+                        column: x => x.CorretoraId,
+                        principalTable: "Corretoras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Acoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ticker = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmpresaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Acoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Acoes_Empresas_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Ordens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NotaId = table.Column<int>(type: "int", nullable: false),
+                    DataHora = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    TipoOrdem = table.Column<string>(type: "varchar(1)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AcaoId = table.Column<int>(type: "int", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    PrecoUnitario = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ordens_Acoes_AcaoId",
+                        column: x => x.AcaoId,
+                        principalTable: "Acoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ordens_Notas_NotaId",
+                        column: x => x.NotaId,
+                        principalTable: "Notas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Acoes_EmpresaId",
+                table: "Acoes",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notas_CorretoraId",
+                table: "Notas",
+                column: "CorretoraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordens_AcaoId",
+                table: "Ordens",
+                column: "AcaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordens_NotaId",
+                table: "Ordens",
+                column: "NotaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ordens");
+
+            migrationBuilder.DropTable(
+                name: "Acoes");
+
+            migrationBuilder.DropTable(
+                name: "Notas");
+
+            migrationBuilder.DropTable(
+                name: "Empresas");
+
+            migrationBuilder.DropTable(
+                name: "Corretoras");
+        }
+    }
+}
