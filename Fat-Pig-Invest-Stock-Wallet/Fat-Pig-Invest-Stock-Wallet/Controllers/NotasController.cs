7699a8e4aba1c8b9b167e984a339c59b7cc84329
Sérgio Lacerda <cursos@sergiolacerda.com.br>
2022-07-13 17:09:23 -0300

    Adding base controllers

---
+++ Fat-Pig-Invest-Stock-Wallet/Fat-Pig-Invest-Stock-Wallet/Controllers/NotasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fat_Pig_Invest_Stock_Wallet.Data;
+using Fat_Pig_Invest_Stock_Wallet.Models;
+
+namespace Fat_Pig_Invest_Stock_Wallet.Controllers
+{
+    public class NotasController : Controller
+    {
+        private readonly FatPigInvestContext _context;
+
+        public NotasController(FatPigInvestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notas
+        public async Task<IActionResult> Index()
+        {
+            var fatPigInvestContext = _context.Notas.Include(n => n.Corretora);
+            return View(await fatPigInvestContext.ToListAsync());
+        }
+
+        // GET: Notas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Notas == null)
+            {
+                return NotFound();
+            }
+
+            var nota = await _context.Notas
+                .Include(n => n.Corretora)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (nota == null)
+            {
+                return NotFound();
+            }
+
+            return View(nota);
+        }
+
+        // GET: Notas/Create
+        public IActionResult Create()
+        {
+            ViewData["CorretoraId"] = new SelectList(_context.Corretoras, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Notas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CorretoraId,NumeroNota,DataPregao,PrecoUnitario,Emolumentos,Corretagem")] Nota nota)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nota);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CorretoraId"] = new SelectList(_context.Corretoras, "Id", "Nome", nota.CorretoraId);
+            return View(nota);
+        }
+
+        // GET: Notas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Notas == null)
+            {
+                return NotFound();
+            }
+
+            var nota = await _context.Notas.FindAsync(id);
+            if (nota == null)
+            {
+                return NotFound();
+            }
+            ViewData["CorretoraId"] = new SelectList(_context.Corretoras, "Id", "Nome", nota.CorretoraId);
+            return View(nota);
+        }
+
+        // POST: Notas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CorretoraId,NumeroNota,DataPregao,PrecoUnitario,Emolumentos,Corretagem")] Nota nota)
+        {
+            if (id != nota.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nota);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotaExists(nota.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CorretoraId"] = new SelectList(_context.Corretoras, "Id", "Nome", nota.CorretoraId);
+            return View(nota);
+        }
+
+        // GET: Notas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Notas == null)
+            {
+                return NotFound();
+            }
+
+            var nota = await _context.Notas
+                .Include(n => n.Corretora)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (nota == null)
+            {
+                return NotFound();
+            }
+
+            return View(nota);
+        }
+
+        // POST: Notas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Notas == null)
+            {
+                return Problem("Entity set 'FatPigInvestContext.Notas'  is null.");
+            }
+            var nota = await _context.Notas.FindAsync(id);
+            if (nota != null)
+            {
+                _context.Notas.Remove(nota);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotaExists(int id)
+        {
+          return (_context.Notas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
